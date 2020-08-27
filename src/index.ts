a37629c729e507fca6ce8f373d00ce626bf641a3
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2020-08-27 17:02:27 +0200

    Include the full feature set

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import * as logger from "koa-logger";
 import * as dotenv from "dotenv";
 
 import { loadJwtMiddleware } from "./jwt-middleware";
+import { tokenToHeaders } from "./token-to-headers";
+import { isTruthy } from "./util";
 
 type TokenData = Record<string, unknown>;
 
@@ -17,15 +19,32 @@ const router = new Router();
 
     app.use(await loadJwtMiddleware());
 
-    router.get("/", (ctx: Koa.ParameterizedContext<{ user: TokenData }>) => {
-        ctx.body = "";
-        console.log(ctx.state.user);
-    });
+    const headerPrefix = process.env.HEADER_PREFIX ?? "X-Auth-";
+    console.log("Header prefix:", headerPrefix);
+
+    // Main authentication route.
+    router.get(
+        "/",
+        (ctx: Koa.ParameterizedContext<{ user: TokenData | undefined }>) => {
+            ctx.body = "";
+            if (ctx.state.user) {
+                ctx.set(tokenToHeaders(ctx.state.user, { headerPrefix }));
+            } else if (ctx.headers.authorization) {
+                // It's OK if you don't pass an authorization header if REQUIRE_TOKEN is not true.
+                // But passing an invalid authorization header? That's forbidden.
+                ctx.status = 403;
+            }
+        }
+    );
+
+    // Health check.
     router.get("/_health", (ctx) => {
         ctx.body = "OK";
     });
 
-    app.use(logger());
+    if (isTruthy(process.env.LOG_REQUESTS)) {
+        app.use(logger());
+    }
     app.use(router.middleware());
 
     const port = parseInt(process.env.PORT ?? "3000");
---
+++ src/token-to-headers.ts
@@ -0,0 +1,60 @@
+interface TokenToHeadersOptions {
+    headerPrefix: string;
+}
+
+export function tokenToHeaders(
+    data: Record<string, unknown>,
+    options: TokenToHeadersOptions
+): Record<string, string> {
+    const output: Record<string, string> = {};
+    function processObject(prefix: string, obj: Record<string, unknown>) {
+        Object.entries(obj).forEach(([k, v]) => {
+            const formattedKey = formatKey(k);
+            const formattedValue = formatSimpleValue(v);
+            if (formattedValue === null) {
+                if (isObject(v)) {
+                    processObject(prefix + formattedKey + ".", v);
+                }
+            } else {
+                output[prefix + formattedKey] = formattedValue;
+            }
+        });
+    }
+    processObject(options.headerPrefix, data);
+    return output;
+}
+
+function formatKey(key: string): string {
+    return key
+        .split(/[^A-Za-z0-9]+/)
+        .map(upperCaseFirst)
+        .join("-");
+}
+
+function formatSimpleValue(value: unknown): string | null {
+    if (value === null || value === undefined) {
+        return "";
+    }
+    if (
+        typeof value === "string" ||
+        typeof value === "number" ||
+        typeof value === "boolean"
+    ) {
+        return value.toString();
+    }
+    if (Array.isArray(value)) {
+        return value
+            .map(formatSimpleValue)
+            .filter((x) => x !== null)
+            .join(",");
+    }
+    return null;
+}
+
+function upperCaseFirst(str: string): string {
+    return str.substr(0, 1).toUpperCase() + str.substr(1);
+}
+
+function isObject(obj: unknown): obj is Record<string, unknown> {
+    return obj && typeof obj === "object";
+}
